48f463b2e32d0f946200fa1faed72e9767f281d6
Javier Segovia Córdoba <javiersegoviacordoba@gmail.com>
2020-12-11 19:13:13 +0100

    Add AGP and Compose to buildscript

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,21 @@
 buildscript {
     val kotlinVersion by System.getProperties()
     val dokkaVersion = "0.10.1"
+    val androidVersion = "4.0.1"
+    val composeVersion = "0.3.0-build134"
 
     repositories {
         mavenCentral()
         jcenter()
+        google()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("org.jetbrains.dokka:dokka-gradle-plugin:$dokkaVersion")
+        classpath("org.jetbrains.compose:compose-gradle-plugin:$composeVersion")
+        classpath("com.android.tools.build:gradle:$androidVersion")
     }
 
 }
