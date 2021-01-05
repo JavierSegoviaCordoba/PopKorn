760278f3feecd071b1634911ccce07dbb97f26ef
Javier Segovia Córdoba <javiersegoviacordoba@gmail.com>
2021-01-05 12:47:38 +0100

    Share Android configuration in `buildSrc`

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 buildscript {
     val kotlinVersion by System.getProperties()
     val dokkaVersion = "0.10.1"
-    val androidVersion = "4.0.1"
 
     repositories {
         mavenCentral()
@@ -12,7 +11,6 @@ buildscript {
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("org.jetbrains.dokka:dokka-gradle-plugin:$dokkaVersion")
-        classpath("com.android.tools.build:gradle:$androidVersion")
     }
 }
 
--- popkorn-androidx-viewmodel/build.gradle.kts
+++ popkorn-androidx-viewmodel/build.gradle.kts
@@ -59,25 +59,4 @@ kotlin {
     }
 }
 
-android {
-    compileSdkVersion(30)
-
-    defaultConfig {
-        minSdkVersion(21)
-        targetSdkVersion(30)
-        versionCode = 1
-        versionName = "1.0"
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-
-    sourceSets.all {
-        java.srcDirs("src/android${name.capitalize()}/kotlin")
-        res.srcDirs("src/android${name.capitalize()}/res")
-        resources.srcDirs("src/android${name.capitalize()}/resources")
-        manifest.srcFile("src/android${name.capitalize()}/AndroidManifest.xml")
-    }
-}
+android.androidConfig()
